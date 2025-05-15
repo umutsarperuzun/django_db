293b17e92bd3310e3a601a52c8ca040419723a3e
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-15 14:18:18 +0100

    Register app and apply initial database migrations

--- django_db/db_app/models.py
+++ django_db/db_app/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class Footballers(models.Model):
     name=models.CharField(max_length=40)
+    position=models.CharField(max_length=50)
+    age=models.IntegerField()
