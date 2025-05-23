4839dff0fff141bbd6ced98eb6c55980e67e65fc
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-23 20:35:55 +0100

    Set up routing, views, and template to display footballer data

--- django_db/db_app/views.py
+++ django_db/db_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def index(request):
+    footballer_data=models.Footballers.objects.all()
+    footballer_context={"footballers":footballer_data}
+    return render(request,'db_app/index.html',context=footballer_context)
