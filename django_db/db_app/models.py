04ada9bc76320c0bf3673b00a8744315d48298b5
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-23 19:34:58 +0100

    Improve model readability with __str__ method and test query operations in shell

--- django_db/db_app/models.py
+++ django_db/db_app/models.py
@@ -5,3 +5,6 @@ class Footballers(models.Model):
     name=models.CharField(max_length=40)
     position=models.CharField(max_length=50)
     age=models.IntegerField()
+    
+    def __str__(self):
+        return f"Name: {self.name}, Position: {self.position} , Age: {self.age}"
