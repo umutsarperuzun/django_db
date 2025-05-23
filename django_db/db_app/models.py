9ebcc549ef713cb80169e699e2fd5cecd225f251
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-23 20:14:17 +0100

    Enhance Footballers model with age and salary validation constraints

--- django_db/db_app/models.py
+++ django_db/db_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator
 
 # Create your models here.
 class Footballers(models.Model):
     name=models.CharField(max_length=40)
     position=models.CharField(max_length=50)
-    age=models.IntegerField()
+    age=models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(150)])
+    salary=models.IntegerField(default=0,validators=[MinValueValidator(0)])
     
     def __str__(self):
-        return f"Name: {self.name}, Position: {self.position} , Age: {self.age}"
+        return f"Name: {self.name}, Position: {self.position} , Age: {self.age} , Salary: {self.salary}"
